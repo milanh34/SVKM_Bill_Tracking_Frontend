d238f25f77bf0e722bb64132d167637e3b434afa
milanh34 <milanh344@gmail.com>
2025-03-26 17:59:54 +0530

    Login CSS fixes

--- src/pages_tailwind/Login.jsx
+++ src/pages_tailwind/Login.jsx
@@ -52,11 +52,11 @@ const LoginPage = () => {
       <LoginHeader />
       <div className="flex flex-row justify-center items-center gap-4 sm:gap-6 lg:gap-8 px-4 sm:px-6 pb-2 sm:pb-3 h-full">
         <div className="w-full lg:w-2/5 xl:w-1/2 bg-[#F3F3F3] rounded-xl p-3 sm:p-5 lg:p-8 mb-4 lg:mb-0 order-2 lg:order-1 h-[50vh]">
-          <h2 className="text-lg sm:text-2xl lg:text-3xl font-bold text-[#01073F]">
+          <h2 className="text-lg sm:text-xl lg:text-2xl xl:text-3xl font-bold text-[#01073F]">
             About Us
           </h2>
           <br className="hidden sm:block" />
-          <p className="text-[#595959] font-medium text-xs sm:text-sm lg:text-base">
+          <p className="text-[#595959] font-medium text-xs sm:text-sm lg:text-sm xl:text-base">
             Shri Vile Parle Kelavani Mandal is a Public Charitable Trust
             registered under the Society's Registration Act and Bombay Public
             Trust Act. From its humble beginnings in 1934, when it took over the
@@ -66,9 +66,9 @@ const LoginPage = () => {
           </p>
         </div>
 
-        <div className="w-full h-full sm:w-5/6 md:w-3/4 lg:w-1/2 xl:w-2/5 border-2 border-gray-300/50 rounded-2xl bg-white flex flex-col items-center justify-between p-4 sm:p-6 gap-4 sm:gap-6 shadow-sm order-1 lg:order-2">
+        <div className="w-full h-full sm:w-5/6 md:w-3/4 lg:w-1/2 xl:w-2/5 border-2 border-gray-300/50 rounded-2xl bg-white flex flex-col items-center justify-between p-4 xl:p-6 gap-4 sm:gap-6 shadow-sm order-1 lg:order-2">
           <div className="flex flex-col items-center w-full justify-around h-full">
-            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-[#01073F] text-center">
+            <h1 className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold text-[#01073F] text-center">
               Login to your account
             </h1>
 
@@ -77,7 +77,7 @@ const LoginPage = () => {
                 <select
                   value={selectedRole}
                   onChange={handleRoleChange}
-                  className="w-full bg-white text-gray-600 py-2 px-4 rounded-xl text-base hover:cursor-pointer appearance-none outline-none"
+                  className="w-full bg-white text-gray-600 xl:py-2 xl:px-4 py-1 px-3 rounded-xl text-base hover:cursor-pointer appearance-none outline-none"
                 >
                   <option value="" disabled hidden>
                     Select Role
@@ -95,18 +95,18 @@ const LoginPage = () => {
             </div>
           </div>
 
-          <div className="w-full sm:w-11/12 lg:w-5/6 border-2 border-gray-300/50 rounded-xl p-3 sm:p-5">
+          <div className="w-full sm:w-11/12 lg:w-5/6 border-2 border-gray-300/50 rounded-xl p-3 xl:p-5">
             <div className="w-full sm:w-11/12 lg:w-10/12 mx-auto">
-              <div className="mb-6 flex items-center">
+              <div className="xl:mb-6 lg:mb-3 sm:mb-2 flex items-center">
                 <img
                   src={email}
                   alt="email"
-                  className="w-6 h-6 flex-shrink-0"
+                  className="xl:w-6 xl:h-6 sm:h-4 sm:w-4 flex-shrink-0"
                 />
                 <input
                   type="email"
                   placeholder="Email"
-                  className="ml-3 w-full border-b border-[#011A99] focus:outline-none text-[#01073F] text-base sm:text-lg py-1"
+                  className="ml-3 w-full border-b border-[#011A99] focus:outline-none text-[#01073F] text-base xl:text-lg py-1"
                 />
               </div>
 
@@ -114,12 +114,12 @@ const LoginPage = () => {
                 <img
                   src={password}
                   alt="password"
-                  className="w-6 h-6 flex-shrink-0"
+                  className="xl:w-6 xl:h-6 sm:h-4 sm:w-4 flex-shrink-0"
                 />
                 <input
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
-                  className="ml-3 w-full border-b border-[#011A99] focus:outline-none text-[#01073F] text-base sm:text-lg py-1"
+                  className="ml-3 w-full border-b border-[#011A99] focus:outline-none text-[#01073F] text-base xl:text-lg py-1"
                 />
                 <div
                   onClick={togglePasswordVisibility}
@@ -128,34 +128,34 @@ const LoginPage = () => {
                   <img
                     src={eye}
                     alt="toggle password visibility"
-                    className="w-5 h-5"
+                    className="xl:w-5 xl:h-5 sm:h-4 sm:w-4"
                   />
                 </div>
               </div>
 
               <div className="flex justify-end mt-3">
-                <p className="text-[#011A99] text-sm sm:text-base cursor-pointer hover:text-[#021678]">
+                <p className="text-[#011A99] text-xs sm:text-sm xl:text-base cursor-pointer hover:text-[#021678]">
                   forgot password?
                 </p>
               </div>
 
-              <div className="mt-8 sm:mt-10">
-                <div className="flex items-start mb-5">
+              <div className="mt-4 sm:mt-6 lg:mt-8 xl:mt-10">
+                <div className="flex items-start mb-1 lg:mb-3 xl:mb-5">
                   <input
                     type="checkbox"
                     id="terms"
-                    className="appearance-none w-4 h-4 rounded-full border-2 border-[#011A99] checked:bg-[#011A99] mt-1 mr-2 flex-shrink-0"
+                    className="appearance-none xl:w-4 xl:h-4 w-3 h-3 rounded-full border-2 border-[#011A99] checked:bg-[#011A99] mt-1 mr-2 flex-shrink-0"
                   />
                   <label
                     htmlFor="terms"
-                    className="text-[#011A99] text-sm sm:text-base"
+                    className="text-[#011A99] text-xs sm:text-sm xl:text-base"
                   >
                     I agree with the terms and conditions.
                   </label>
                 </div>
 
                 <button
-                  className="w-full bg-[#011A99] text-white rounded-2xl py-2 sm:py-3 font-semibold hover:bg-[#021678] transition-colors duration-200 cursor-pointer text-sm sm:text-base"
+                  className="w-full bg-[#011A99] text-white rounded-2xl py-2 xl:py-3 font-semibold hover:bg-[#021678] transition-colors duration-200 cursor-pointer text-xs sm:text-sm xl:text-base"
                   onClick={handleLogin}
                 >
                   Login
